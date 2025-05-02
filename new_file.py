0160e1e31c7fd5c628b3ae42b94bc1bf3c6529d7
richytruitt <richyctruitt@gmail.com>
2025-05-02 12:46:45 -0500

    created final version of the python file

---
+++ new_file.py
@@ -0,0 +1,6 @@
+print("This is the new feature we are adding")
+print("I removed the other lines and added these two")
+
+print("This line is ultimately what needs to be kept")
+
+print("creating a merge conflict. but this is the only line it should show")
