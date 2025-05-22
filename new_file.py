dfc79ed537193257fdc87b0e894a88001d263638
richytruitt <richyctruitt@gmail.com>
2025-05-22 08:03:42 -0500

    new changes

--- new_file.py
+++ new_file.py
@@ -4,3 +4,5 @@ print("I removed the other lines and added these two")
 print("This line is ultimately what needs to be kept")
 
 print("creating a merge conflict. but this is the only line it should show")
+
+print("This should make a conflict happen. It can be deleted with my newest changes")
